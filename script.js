d937003a251f2d6c164c71334cffa7bcbf275920
Karun Choudhary <Karunchoudhary2000@gmail.com>
2023-10-21 17:47:27 +0530

    Structure of the game till levels of the game which is selected

---
+++ script.js
@@ -0,0 +1,6 @@
+// JavaScript code goes here
+document.addEventListener("DOMContentLoaded", function () {
+    // Your JavaScript code from the previous example can go here
+    // Make sure to wrap it in an appropriate function or event listener if needed.
+  });
+  
