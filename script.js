74178c3afffa3db137d206f70b4ea88304f895a3
Karun Choudhary <Karunchoudhary2000@gmail.com>
2023-10-21 20:16:34 +0530

    pop up functanility is added to ask 'Are you ready to play with yes or no option'

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-// JavaScript code goes here
+
 document.addEventListener("DOMContentLoaded", function () {
-    // Your JavaScript code from the previous example can go here
-    // Make sure to wrap it in an appropriate function or event listener if needed.
+    
   });
   
