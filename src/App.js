92f2b3cf487dd89d1f3756b1956240c000689b3f
Karun Choudhary <Karunchoudhary2000@gmail.com>
2024-10-05 02:16:27 +0530

    ready to deploy

---
+++ src/App.js
@@ -0,0 +1,42 @@
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/index">Open index.html</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route
+            path="/"
+            element={<h2>Welcome to the React App! Click on the link to open index.html</h2>}
+          />
+          <Route
+            path="/index"
+            element={
+              <iframe
+                title="Index Page"
+                src="/index.html" // Accessing index.html from the public directory
+                style={{ width: '100%', height: '100vh', border: 'none' }}
+              />
+            }
+          />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
+
